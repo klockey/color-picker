418b53d0e7b7117cec96f62cf4e07cba04e60d26
Keith Lockey <lockey@gmail.com>
2017-03-24 14:10:52 -0400

    color-picker

--- public/main.js
+++ public/main.js
@@ -1,6 +1,37 @@
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
+//
+// document.addEventListener('DOMContentLoaded', main)
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  let hueVal = 0
+  let satVal = 0
+  let lightVal = 0
+
+  const hueSlider = document.querySelector('input[name=hue]')
+  const satSlider = document.querySelector('input[name=sat]')
+  const lightSlider = document.querySelector('input[name=light]')
+  const number = document.querySelector('div.number')
+  const swatch = document.querySelector('div.swatch')
+
+  hueSlider.addEventListener('input', (event) => {
+    hueVal = event.target.value
+    updateColor()
+  })
+  satSlider.addEventListener('input', (event) => {
+    satVal = event.target.value
+    updateColor()
+  })
+  lightSlider.addEventListener('input', (event) => {
+    lightVal = event.target.value
+    updateColor()
+  })
+  const updateColor = () => {
+    let color = `hsl(${hueVal}, ${satVal}%, ${lightVal}%)`
+    number.textContent = color
+    swatch.style.backgroundColor = color
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
